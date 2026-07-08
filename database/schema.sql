c384b91ac8e9318e819c8c2d282f35589d1075a1
Reshma <reshmakumari8252rk@gmail.com>
2026-07-08 21:26:24 +0530

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,1 +1,9 @@
+-- ============================================
+-- NovaCart AI Database Schema
+-- Version : 1.0
+-- Author  : Reshma Kumari
+-- ============================================
 
+CREATE DATABASE IF NOT EXISTS NovaCart_AI;
+
+USE NovaCart_AI;
